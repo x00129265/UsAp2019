d0cea3ee48ed8079e26c42171d0126a3ff86b07f
jtihonovs <jtihonovs@transperfect.com>
2019-07-08 21:18:32 +0100

    Created models

---
+++ UsAp2019/Models/Ad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsAp2019.Models
+{
+    public class Ad
+    {
+        [ForeignKey("User")]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ UsAp2019/Models/Target.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsAp2019.Models
+{
+    public enum Sex
+    {
+        Male = 0, Female = 1 // There is only 2 genders
+    }
+    public class Target
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public int AgeFrom { get; set; }
+        public int AgeTo { get; set; }
+        public Sex Gender { get; set; }
+        public virtual Ad TargetAd { get; set; }
+    }
+}
